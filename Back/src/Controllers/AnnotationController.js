16291d3cfd134d872a6a23c48f44744b60415e59
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-03-22 12:08:10 -0300

    Adiciona logs e validação na função de leitura e criação de anotações

--- Back/src/Controllers/AnnotationController.js
+++ Back/src/Controllers/AnnotationController.js
@@ -3,15 +3,25 @@ const Annotations = require('./../models/AnnotationData');
 
 module.exports = {
     async read(req, res) {  
+      console.log("read function called"); // Log when the function is called
       const annotationList = await Annotations.find(); // Nesa linha estou buscando todos os registros do banco de dados
+      console.log("Annotations.find() returned", annotationList); // Log the result of Annotations.find()
 
-      return responde.json(annotationList); // Nessa linha estou retornando a lista de anotações    
+      return res.json(annotationList); // Nessa linha estou retornando a lista de anotações    
     },
-    create(req, res) {
+    async create(req, res) {
        const { title, notas, priority } = req.body;  // Nessa linha estou extraindo os dados do corpo da requisição 
+
+       if(!notas || !title){
+        return res.status(400).json({error: "Necessario um titulo e uma anotação!"});
+       }
        
-       console.log(title);
-       console.log(notas);
-       console.log(priority);   
+        const annotationCreated = await Annotations.create({
+            title,
+            notas,
+            priority
+        }); // Nessa linha estou criando um novo registro no banco de dados
+      
+        return res.json(annotationCreated); // Nessa linha estou retornando a anotação criada 
     }  
 }
