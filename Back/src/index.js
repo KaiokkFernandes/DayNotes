b698e716326fb3bee07f3ad5dd6cf73e8423e5bb
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-03-24 17:16:09 -0300

    Adiciona dependência CORS e middleware para CORS

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,11 +1,14 @@
 
 const express = require('express');
 const routes = require('./routes.ts'); // Ajuste o caminho para 'routes.js'
+const cors = require('cors');
 
 const app = express();
 
 require('./database/dbConfig.js'); // Conexão com o banco de dados
 
+
+app.use(cors()); // Middleware para CORS
 app.use(express.json()); // Middleware para JSON
 app.use(routes); // Implementação das rotas
 
