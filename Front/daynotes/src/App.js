45609dffdaa163c391f659bd691039fdde576c14
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-03-31 21:23:51 -0300

    Adiciona dependência do Axios e corrige importações

--- Front/daynotes/src/App.js
+++ Front/daynotes/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';  
+import React, { useState } from 'react'
 
-import React from 'react';  
 import './globalcss.css';
 import './sidebar.css';
 import './App.css';
@@ -11,11 +10,19 @@ import Notes from './components/Notes/index.js ';
 function App() {
   const  [title, setTitles] = useState('');
   const  [notas, setNatas] = useState('');  
+
+  function handleSubmit(e){
+    e.preventDefault();
+     
+  } 
+
+
+
   return (
     <div>
       <aside>
         <strong>Caderno de Notas</strong>
-        <form>
+        <form  onSubmit={handleSubmit}>
           <div className='int-block'>
             <label htmlFor='title'>Título de Anotação</label>
             <input 
